d66932470b6ebba32d863e16c55839daf638183e
Shanaforth <3602121+Shanaforth@users.noreply.github.com>
2025-01-19 00:37:31 +0000

    Update based on main commit c22f7501ef12ef906c26f0dc11f262cfd9bf4eaa

--- out/example.js
+++ out/example.js
@@ -19295,35 +19295,45 @@
       (function() {
         "use strict";
         var hasOwn = {}.hasOwnProperty;
-        var nativeCodeString = "[native code]";
         function classNames17() {
-          var classes = [];
+          var classes = "";
           for (var i = 0; i < arguments.length; i++) {
             var arg = arguments[i];
-            if (!arg) continue;
-            var argType = typeof arg;
-            if (argType === "string" || argType === "number") {
-              classes.push(arg);
-            } else if (Array.isArray(arg)) {
-              if (arg.length) {
-                var inner = classNames17.apply(null, arg);
-                if (inner) {
-                  classes.push(inner);
-                }
-              }
-            } else if (argType === "object") {
-              if (arg.toString !== Object.prototype.toString && !arg.toString.toString().includes("[native code]")) {
-                classes.push(arg.toString());
-                continue;
-              }
-              for (var key in arg) {
-                if (hasOwn.call(arg, key) && arg[key]) {
-                  classes.push(key);
-                }
-              }
+            if (arg) {
+              classes = appendClass(classes, parseValue(arg));
             }
           }
-          return classes.join(" ");
+          return classes;
+        }
+        function parseValue(arg) {
+          if (typeof arg === "string" || typeof arg === "number") {
+            return arg;
+          }
+          if (typeof arg !== "object") {
+            return "";
+          }
+          if (Array.isArray(arg)) {
+            return classNames17.apply(null, arg);
+          }
+          if (arg.toString !== Object.prototype.toString && !arg.toString.toString().includes("[native code]")) {
+            return arg.toString();
+          }
+          var classes = "";
+          for (var key in arg) {
+            if (hasOwn.call(arg, key) && arg[key]) {
+              classes = appendClass(classes, key);
+            }
+          }
+          return classes;
+        }
+        function appendClass(value, newClass) {
+          if (!newClass) {
+            return value;
+          }
+          if (value) {
+            return value + " " + newClass;
+          }
+          return value + newClass;
         }
         if (typeof module !== "undefined" && module.exports) {
           classNames17.default = classNames17;
